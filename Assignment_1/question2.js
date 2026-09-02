749a4c47344dab181ffed4c0aee063978b7220ec
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-09-02 20:04:55 +0600

    add: implement getDayType function to classify days as Weekend or Working Day

---
+++ Assignment_1/question2.js
@@ -0,0 +1,26 @@
+// Question No. 2
+function getDayType(day) {
+    const result = day.toLowerCase();
+
+    switch (result) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
+console.log(getDayType("Friday"));
+console.log(getDayType("Sunday"));
+console.log(getDayType("Monday"));
+console.log(getDayType("Saturday"));
+console.log(getDayType("InvalidDay"));
