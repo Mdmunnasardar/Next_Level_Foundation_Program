f2c8854f3603c87f7eeb4e4dad5d9b484db6acc2
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2026-09-02 20:17:50 +0600

    add: implement getCngFare function for fare calculation based on distance, night time, and waiting minutes

--- Assignment_1/answer.js
+++ Assignment_1/answer.js
@@ -45,3 +45,20 @@ function validateUsername(username) {
     }
 }
 
+
+// question 4 of main code and you can see the code is same as question4.js and can test the function by calling it with different distance, isNight and waitingMinutes values.
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+
+    return fare;
+}
